dc54db714a02ce555ddb4c58fb2c118defa0c4ae
Marcin Gębala <maarcin.gebala@gmail.com>
2020-07-15 08:37:29 +0200

    Only GraphQL endpoint

--- ariadne_demo/app.py
+++ ariadne_demo/app.py
@@ -1,16 +1,8 @@
 from ariadne.asgi import GraphQL
 from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.routing import Route
 
 from .schema import schema
 
 
-async def hello(*args, **kwargs):
-    return JSONResponse({"Hello": "World"})
-
-
-routes = [Route("/", hello), Route("/graphql", GraphQL(schema=schema, debug=True))]
-
-app = Starlette(routes=routes, debug=True)
-
+app = Starlette(debug=True)
+app.mount("/", GraphQL(schema=schema, debug=True))
